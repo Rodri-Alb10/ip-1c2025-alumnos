fb23dcb4c73a9f95078399515bd4e8509a2594e9
Rodri-Alb10 <piratarodrigo2612@gmail.com>
2025-06-20 21:14:29 -0300

    Update views.py
    Se modifica views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 # capa de vista/presentación
 
 from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from .forms import SubscribeForm
+from app.layers.services.services import register_user
 
 def index_page(request):
     return render(request, 'index.html')
@@ -12,16 +18,45 @@ def index_page(request):
 def home(request):
     images = []
     favourite_list = []
+    favourite_list_name = []
 
-    return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+    images = services.getAllImages()
+    favourite_list = services.getAllFavourites(request)
+    for pokemon in favourite_list:
+        favourite_list_name.append(pokemon.name)
+    return render(request, 'home.html', { 'images': images, 'favourite_list_name': favourite_list_name })
+       
+# esta funcion envía un mail al usuario al registrarse
 
+def subscribe(request):
+    form = SubscribeForm()
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data
+            errores = register_user(form.cleaned_data)
+
+            if errores:
+                for error in errores:
+                    messages.error(request,error)
+            else:
+                username = usuario['username']
+                email = usuario['email']
+                password = usuario['password']
+                subject = 'Registro exitoso'
+                message = f'¡Gracias por registrarte {username}!\nEstas son tus credenciales de inicio de sesión:\nUsuario:{username}\nContraseña:{password}'
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [email], fail_silently=False)
+                return redirect('loading_home')            
+    return render(request, 'registration/register.html', {'form': form})
+
+    
 # función utilizada en el buscador.
 def search(request):
     name = request.POST.get('query', '')
 
     # si el usuario ingresó algo en el buscador, se deben filtrar las imágenes por dicho ingreso.
     if (name != ''):
-        images = []
+        images = services.filterByCharacter(name)
         favourite_list = []
 
         return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
@@ -33,7 +68,7 @@ def filter_by_type(request):
     type = request.POST.get('type', '')
 
     if type != '':
-        images = [] # debe traer un listado filtrado de imágenes, segun si es o contiene ese tipo.
+        images = services.filterByType(type) # debe traer un listado filtrado de imágenes, segun si es o contiene ese tipo.
         favourite_list = []
 
         return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
@@ -43,15 +78,23 @@ def filter_by_type(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    pass
+    favourite_list = []    
+    images = []
+    favourite_list = services.getAllFavourites(request)
+
+
+    return render(request, 'favourites.html', { 'images': images, 'favourite_list': favourite_list })
+     
 
 @login_required
 def saveFavourite(request):
-    pass
+    services.saveFavourite(request)
+    return redirect('favoritos')
 
 @login_required
 def deleteFavourite(request):
-    pass
+    services.deleteFavourite(request)
+    return redirect('favoritos')
 
 @login_required
 def exit(request):
